e4ba68d36b77220fcb95a25348fb726415c8e6ed
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-12 05:47:02 +0000

    １

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    <h1>about Page home</h1>
-    <nuxt-link to="/">戻る</nuxt-link>
-  </div>
+  <layout-normal>
+    <template #main>
+      <div>
+        <h1>about Page home</h1>
+        <nuxt-link to="/">戻る</nuxt-link>
+      </div>
+    </template>
+  </layout-normal>
 </template>
+
+<script lang="ts">
+import LayoutNormal from '@/layouts/LayoutNormal.vue';
+
+export default {
+  layout: LayoutNormal,
+};
+</script>
