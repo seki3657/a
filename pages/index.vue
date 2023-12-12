126f511f4a2217055e10aec8715519f36b34d9b9
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-12 05:08:46 +0000

    1

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="box">
+  <div>
     <h1>index Page「ようこそ」</h1>
-    <nuxt-link to="/about">About</nuxt-link>
-  </div>
-  <div class="box">
-    <nuxt-link to="/recipe">Recipe</nuxt-link>
+
+    <div class="box">
+      <nuxt-link to="/about">About</nuxt-link>
+    </div>
+
+    <div class="box">
+      <nuxt-link to="/recipe">Recipe</nuxt-link>
+    </div>
   </div>
 </template>
 
