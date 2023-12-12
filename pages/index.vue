de06aa19daed35a44cc32a229ec2241467d7393b
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-12 19:44:42 +0000

    1

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,74 @@
 <template>
   <div>
-  <h1>世界文化遺産人気ランキング</h1>
+    <h1 class="mb-4">世界文化遺産人気ランキング</h1>
+
+    <div class="card mb-4">
+      <div class="card-body">
+        <h2 class="card-title">
+          <nuxt-link to="/about">１位・タージ・マハル</nuxt-link>
+        </h2>
+        <img
+          src="https://thumb.photo-ac.com/a5/a5506706c92c2c1c686c034e56403e1d_t.jpeg"
+          alt="Recipe Image"
+          class="img-fluid rounded"
+        />
+      </div>
+    </div>
+
+    <div class="box">
+      <nuxt-link to="/recipe" class="btn btn-primary">Recipe</nuxt-link>
+    </div>
   </div>
-  <div>
-    <h2><nuxt-link to="/about">１位・タージ・マハル</nuxt-link></h2>
-    <img
-      src="https://thumb.photo-ac.com/a5/a5506706c92c2c1c686c034e56403e1d_t.jpeg"
-      alt="Recipe Image"
-    />
-  </div>
-  <div>  <nuxt-link to="/recipe">Recipe</nuxt-link>
-  </div>
-  </template>
-  <style scoped>
-  body {
-    background-color: skyblue;
-  }
-   
-  .box {
-    margin: 0 auto;
-    width: 100px;
-    height: 100px;
-    background-color: yellow;
-  }
-  </style>
+</template>
+
+<style scoped>
+body {
+  background-color: #87CEEB; /* skyblueの代わりにBootstrapのcolor名を使用 */
+}
+
+.box {
+  margin: 0 auto;
+  width: 100px;
+  height: 100px;
+  background-color: yellow;
+}
+
+/* このスタイルはBootstrapのスタイルを上書きする例です */
+.card {
+  border: 1px solid #ddd; /* カードのボーダーを追加 */
+}
+
+.card-title {
+  color: #333; /* カードのタイトルの色を変更 */
+}
+</style>
 
 <script setup>
 const data = useRecipe()
 </script>
 
+<!-- BootstrapのCSSとJavaScriptをhead内に追加 -->
+<script>
+export default {
+  head() {
+    return {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+          integrity: 'sha384-GLhlTQ8iKII+UcCOJ3mOefGtr5lqQ8z5nZl5cbz5l5u0jBRbbC6LHEz1ke2bTII',
+          crossorigin: 'anonymous',
+        },
+      ],
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js',
+          integrity: 'sha384-eMMyWiaWqDzx9OyDD10aysoA9/z9qdyY/dP0rlkyiMDzvqJb4FU/XjIWrPTdWl+P',
+          crossorigin: 'anonymous',
+          defer: true,
+        },
+      ],
+    };
+  },
+};
+</script>
