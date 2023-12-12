ca55befbda9ff1f828558dbced4f0afdc1d5e242
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-12 05:02:20 +0000

    1

--- pages/index.vue
+++ pages/index.vue
@@ -6,3 +6,15 @@
   <div>  <nuxt-link to="/recipe">Recipe</nuxt-link>
   </div>
 </template>
+<style scoped>
+body {
+  background-color: skyblue;
+}
+
+.box {
+  margin: 0 auto;
+  width: 100px;
+  height: 100px;
+  background-color: yellow;
+}
+</style>
