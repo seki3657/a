7570e787e100fa6f6083b0e987a34d38dcf11842
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-12 20:02:17 +0000

    1

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,11 @@
   <div>
     <div class="container mt-4">
       <h1 class="mb-4">タージ・マハル</h1>
-      
+      <img
+          src="https://thumb.photo-ac.com/a5/a5506706c92c2c1c686c034e56403e1d_t.jpeg"
+          alt="Recipe Image"
+          class="img-fluid rounded"
+        />
       <div class="mb-4">
         <h3 class="mb-4">タージ・マハルはインド北部のアーグラにある、ムガル帝国第5代皇帝シャー・ジャハーンの妃ムムターズ・マハルの墓廟です。総大理石の白亜の廟は、インド・イスラム文化の代表的な建築物です。1983年、インドの世界遺産に登録されました。</h3>
       </div>
@@ -45,3 +49,4 @@ export default {
   },
 };
 </script>
+
