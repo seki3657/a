01038b8657039713c1da9ed6b5d24383a53fb4e7
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-12 05:31:22 +0000

    １

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
+  <layout-normal>
+    <template #main>
+      <div>
     <h1>about Page home</h1>
     <nuxt-link to="/">戻る</nuxt-link>
   </div>
+    </template>
+  </layout-normal>
 </template>
