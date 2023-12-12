527f017553f2d7d0c3fcaca9cc64adb31955e708
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-12 05:25:42 +0000

    １

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    <h1>about Page home</h1>
-    <nuxt-link to="/">戻る</nuxt-link>
+    <LayoutResizableDrawer>
+      <!-- ドロワーコンテンツ -->
+      <template #drawer>
+        <!-- ここにドロワーコンテンツを挿入 -->
+      </template>
+
+      <!-- メインコンテンツ -->
+      <template #main>
+        <h1>about Page home</h1>
+        <nuxt-link to="/">戻る</nuxt-link>
+      </template>
+    </LayoutResizableDrawer>
   </div>
 </template>
+
+<script>
+import LayoutResizableDrawer from '~/components/LayoutResizableDrawer.vue';
+
+export default {
+  components: {
+    LayoutResizableDrawer,
+  },
+};
+</script>
+
