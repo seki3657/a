38ae4cc7b0b3d79bf0e2bca7d5f3e441751b709e
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-12 05:34:58 +0000

    1

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,20 @@
+<!-- about.vue -->
+
 <template>
   <layout-normal>
     <template #main>
       <div>
-    <h1>about Page home</h1>
-    <nuxt-link to="/">戻る</nuxt-link>
-  </div>
+        <h1>about Page home</h1>
+        <nuxt-link to="/">戻る</nuxt-link>
+      </div>
     </template>
   </layout-normal>
 </template>
+
+<script lang="ts">
+import LayoutNormal from '@/layouts/LayoutNormal.vue';
+
+export default {
+  layout: LayoutNormal,
+};
+</script>
---
+++ pages/layoutNormal.vue
@@ -0,0 +1,23 @@
+<!-- LayoutNormal.vue -->
+
+<template>
+  <div>
+    <v-container>
+      <slot name="main">
+        <!-- メインコンテンツが入る -->
+      </slot>
+    </v-container>
+    <my-footer />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+import MyFooter from '@/components/my-footer.vue';
+
+/**
+ * 通常のレイアウト
+ */
+@Component({ components: { MyFooter } })
+export default class LayoutNormal extends Vue {}
+</script>
