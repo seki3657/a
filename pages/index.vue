b100f92c554114bf21a49a78ddbe5a42cc794fd5
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-12 19:07:01 +0000

    a

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,22 @@
-<!-- pages/index.vue -->
 <template>
   <div>
-    <h1>index Page「ようこそ」</h1>
-
-    <div class="box">
-      <nuxt-link to="/about">About</nuxt-link>
-    </div>
-
-    <div class="box">
-      <nuxt-link to="/recipe">Recipe</nuxt-link>
-    </div>
+  <h1>index Page「ようこそ」</h1>
+  <nuxt-link to="/about">About</nuxt-link>
   </div>
-</template>
-
-<script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator';
-
-/**
- * ページコンポーネント
- */
-@Component({ layout: 'layout-custom' })
-export default class IndexPage extends Vue {}
-</script>
-
-<style scoped>
-body {
-  background-color: skyblue;
-}
-
-.box {
-  margin: 10 auto;
-  width: 1000px;
-  height: 100px;
-  background-color: yellow;
-}
-</style>
+  <div>  <nuxt-link to="/recipe">Recipe</nuxt-link>
+  </div>
+  </template>
+  <style scoped>
+  body {
+    background-color: skyblue;
+  }
+   
+  .box {
+    margin: 0 auto;
+    width: 100px;
+    height: 100px;
+    background-color: yellow;
+  }
+  </style>
 
 
