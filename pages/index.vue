59430ab9e4e2964f761fa6add5d2b853a26d4cc1
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-12 04:45:41 +0000

    あ

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,6 @@
     <h1>index Page「ようこそ」</h1>
     <nuxt-link to="/">Home</nuxt-link>
     <nuxt-link to="/about">About</nuxt-link>
-    <nuxt-link to="/recipe">Recipe</nuxt-link> <!-- 新しいリンクを追加 -->
+    <nuxt-link to="/recipe">Recipe</nuxt-link>
   </div>
 </template>
