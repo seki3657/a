137cfc7ecc2da38940bbba8a0c6d38edb7f905cc
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-12 05:17:26 +0000

    １

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,11 @@
   <div>
     <h1>index Page「ようこそ」</h1>
 
-    <div class="box" style="background-color: #90ee90;">
+    <div class="box">
       <nuxt-link to="/about">About</nuxt-link>
     </div>
 
-    <div class="box" style="background-color: #90ee90;">
+    <div class="box">
       <nuxt-link to="/recipe">Recipe</nuxt-link>
     </div>
   </div>
@@ -21,8 +21,7 @@ body {
   margin: 0 auto;
   width: 100px;
   height: 100px;
-  background-color: yellow;
+  background-color: #90ee90; /* ライトグリーンに設定 */
 }
 </style>
-
 
