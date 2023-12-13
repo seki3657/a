3b9a72a3a0f13b0bd88c1655948e41d9bfd567c8
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-13 06:58:33 +0000

    １

---
+++ components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <p>&copy; 2023 Your Website</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  // コンポーネントのロジックがあればここに記述
+};
+</script>
+
+<style scoped>
+/* フッターのスタイリングがあればここに記述 */
+footer {
+  background-color: #333;
+  color: white;
+  padding: 10px;
+  text-align: center;
+}
+</style>
