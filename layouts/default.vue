0d10fec8ff1a35fec7c1910fdb424b43a1fef3a7
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-13 06:47:08 +0000

    1

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
-    <AppHeader />
-    <AppAside />
-    <slot />
-    <AppFooter />
-  </div>
+  <Layout>
+    <p>親コンポーネントで挿入される要素</p>
+  </Layout>
 </template>
+
+<script>
+import Layout from '@/components/Layout.vue';
+
+export default {
+  components: {
+    Layout,
+  },
+};
+</script>
