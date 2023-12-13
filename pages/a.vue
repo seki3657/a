d552afb440b9bb2312f1a6b2abbf72d118da5e22
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-13 04:20:59 +0000

    1

--- pages/a.vue
+++ pages/a.vue
@@ -3,12 +3,12 @@
     <div class="container mt-4">
       <h1 class="mb-4">自由の女神像</h1>
       <img
-          src="https://thumb.photo-ac.com/a5/a5506706c92c2c1c686c034e56403e1d_t.jpeg"
+          src="https://thumb.photo-ac.com/b9/b92f7ddb7e2f121217fea880c31d023c_t.jpeg"
           alt="Recipe Image"
           class="img-fluid rounded"
         />
       <div class="mb-4">
-        <h3 class="mb-4">説明</h3>
+        <h3 class="mb-4">自由の女神像はアメリカのニューヨーク港内リバティ島にあり、1984年、世界遺産に登録されました。 正式名称は「世界を照らす自由」といい、アメリカの自由と民主主義の象徴となっています。</h3>
       </div>
       <div class="mb-4">
         <nuxt-link to="/" class="btn btn-primary">戻る</nuxt-link>
--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@
           <nuxt-link to="/a">2位・自由の女神</nuxt-link>
         </h2>
         <img
-          src="https://thumb.photo-ac.com/a5/a5506706c92c2c1c686c034e56403e1d_t.jpeg"
+          src="https://thumb.photo-ac.com/b9/b92f7ddb7e2f121217fea880c31d023c_t.jpeg"
           alt="Recipe Image"
           class="img-fluid rounded"
         />
