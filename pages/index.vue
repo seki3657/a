3cc2ff8498202ef60d2bb7d42c5bf25b24ac2000
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-17 16:27:49 +0000

    1

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
             <nuxt-link to="/about">１位・タージ・マハル</nuxt-link>
           </h2>
           <img
-            src="https://thumb.photo-ac.com/a5/a5506706c92c2c1c686c034e56403e1d_t.jpeg"
+            :src="require('~/static/images/taj-mahal.jpg')"
             alt="Taj Mahal"
             class="img-fluid rounded"
           />
@@ -22,7 +22,7 @@
             <nuxt-link to="/a">2位・自由の女神</nuxt-link>
           </h2>
           <img
-            src="https://thumb.photo-ac.com/b9/b92f7ddb7e2f121217fea880c31d023c_t.jpeg"
+            :src="require('~/static/images/statue-of-liberty.jpg')"
             alt="Statue of Liberty"
             class="img-fluid rounded"
           />
@@ -34,43 +34,7 @@
       <nuxt-link to="/recipe" class="btn btn-primary stylish-button">Recipe</nuxt-link>
     </div>
   </div>
-  <div>
-    <Header />
-    
-    <main>
-      <h1>トップページ</h1>
-      <p>ここにページコンテンツが入ります。</p>
-
-      <!-- 例: 新しいコンポーネントを追加 -->
-      <MyCustomComponent />
-
-      <!-- 例: テキストの追加 -->
-      <p>新しいテキストを追加します。</p>
-
-      <!-- 例: 画像の追加 -->
-      <img src="~/assets/images/example.jpg" alt="Example Image" />
-
-      <!-- 例: リンクの追加 -->
-      <a href="/new-page">新しいページへのリンク</a>
-    </main>
-    
-    <Footer />
-  </div>
 </template>
-
-<script>
-import Header from '@/components/Header.vue';
-import Footer from '@/components/Footer.vue';
-import MyCustomComponent from '@/components/MyCustomComponent.vue'; // 例として追加
-
-export default {
-  components: {
-    Header,
-    Footer,
-    MyCustomComponent,
-  },
-};
-</script>
 
 <style scoped>
 body {
@@ -106,8 +70,5 @@ body {
 </style>
 
 <script setup>
-const recipe = useRecipe()
-onMounted(() => {
-  // コンポーネントがマウントされた時の処理
-});
+const data = useRecipe()
 </script>
