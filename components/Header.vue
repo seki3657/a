bd673bbbedd55db6904dfa55a4a9f09a9724a54f
seki890 <146801295+seki890@users.noreply.github.com>
2023-12-19 05:18:13 +0000

    1

---
+++ components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <div class="logo">My Logo</div>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+      </ul>
+    </nav>
+    <!-- 他のヘッダー要素を追加 -->
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: #333;
+  color: white;
+  padding: 10px;
+}
+
+.logo {
+  font-size: 1.5em;
+  font-weight: bold;
+}
+
+nav ul {
+  list-style: none;
+  display: flex;
+}
+
+nav li {
+  margin-right
