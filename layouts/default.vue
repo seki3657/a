bb5f06bd987a3eca2213bb1df2addfcd3a115800
seki3657 <158128248+seki3657@users.noreply.github.com>
2024-01-30 06:50:51 +0000

    1

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,1 +1,38 @@
+[15:32] 関 亮太
+<script setup>
 
+import { useMouse } from './mouse.js'
+
+const { x, y } = useMouse()
+
+</script>
+
+<template>Mouse position is at: {{ x }}, {{ y }}</template>
+[15:50] 関 亮太
+<template>
+
+  <div>
+
+    <nuxt />
+
+    <Footer />
+
+  </div>
+
+</template>
+
+<script>
+
+import Footer from '@/components/Footer'
+
+export default {
+
+  components: {
+
+    Footer
+
+  }
+
+}
+
+</script>
