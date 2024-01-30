13fa0e4d0a05debe9c04f01a7e19e65284d03a33
seki3657 <158128248+seki3657@users.noreply.github.com>
2024-01-30 06:49:38 +0000

    1

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,1 @@
-<template>
-  <div>
-    <nuxt />
-    <he />
-  </div>
-</template>
 
-<script>
-import he from '@/components/He'  // Import the correct component
-
-export default {
-  components: {
-    he
-  }
-}
-</script>
