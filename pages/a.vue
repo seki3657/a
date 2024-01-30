f25d213b75b94f8cd2b84733692425d1e77cf5b7
seki3657 <158128248+seki3657@users.noreply.github.com>
2024-01-30 05:39:27 +0000

    1

--- pages/a.vue
+++ pages/a.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <div class="container mt-4">
-      <h1 class="mb-4">{{title}}</h1>
+      <h1 class="mb-4">自由の女神像</h1>
       <img
           src="https://thumb.photo-ac.com/b9/b92f7ddb7e2f121217fea880c31d023c_t.jpeg"
           alt="Recipe Image"
           class="img-fluid rounded"
         />
       <div class="mb-4">
-        <h3 class="mb-4">{{ title }}</h3>
+        <h3 class="mb-4">自由の女神像はアメリカのニューヨーク港内リバティ島にあり、1984年、世界遺産に登録されました。 正式名称は「世界を照らす自由」といい、アメリカの自由と民主主義の象徴となっています。</h3>
       </div>
       <div class="mb-4">
         <nuxt-link to="/" class="btn btn-primary">戻る</nuxt-link>
