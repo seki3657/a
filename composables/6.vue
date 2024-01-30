edcc831530ff781cbe69435b7331acb43a213c23
seki3657 <158128248+seki3657@users.noreply.github.com>
2024-01-30 05:18:40 +0000

    1

--- composables/6.vue
+++ composables/6.vue
@@ -1,11 +1,10 @@
-<script>const ComponentsApp = {
-    data() {
-      return {
-        title:"自由の女神像",
-        title2:"自由の女神像はアメリカのニューヨーク港内リバティ島にあり、1984年、世界遺産に登録されました。 正式名称は「世界を照らす自由」といい、アメリカの自由と民主主義の象徴となっています。",
-      }
-
-        const app = Vue.createApp(ComponentsApp);
-    app.mount("#app");
-        </script>
-
+<script>
+export default {
+  data() {
+    return {
+      title: "自由の女神像",
+      title2: "自由の女神像はアメリカのニューヨーク港内リバティ島にあり、1984年、世界遺産に登録されました。正式名称は「世界を照らす自由」といい、アメリカの自由と民主主義の象徴となっています。",
+    };
+  },
+};
+</script>
