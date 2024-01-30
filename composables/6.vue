d218458c5928f5a71ff47fcdba4dd9939b5873d2
seki3657 <158128248+seki3657@users.noreply.github.com>
2024-01-30 05:14:59 +0000

    1

--- composables/6.vue
+++ composables/6.vue
@@ -1,5 +1,11 @@
 <script>const ComponentsApp = {
     data() {
-        title:"自由の女神像"
-        title2:"自由の女神像はアメリカのニューヨーク港内リバティ島にあり、1984年、世界遺産に登録されました。 正式名称は「世界を照らす自由」といい、アメリカの自由と民主主義の象徴となっています。"
-      return {</script>
+      return {
+        title:"自由の女神像",
+        title2:"自由の女神像はアメリカのニューヨーク港内リバティ島にあり、1984年、世界遺産に登録されました。 正式名称は「世界を照らす自由」といい、アメリカの自由と民主主義の象徴となっています。",
+      }
+
+        const app = Vue.createApp(ComponentsApp);
+    app.mount("#app");
+        </script>
+
