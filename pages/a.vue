28f5aaa576cdad2ea79606275dddbbd4475337f7
seki3657 <158128248+seki3657@users.noreply.github.com>
2024-01-30 05:29:35 +0000

    1

--- pages/a.vue
+++ pages/a.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
     <div class="container mt-4">
-      <h1 class="mb-4">{{title}}</h1>
+      <h1 class="mb-4">{{ title }}</h1>
       <img
-          src="https://thumb.photo-ac.com/b9/b92f7ddb7e2f121217fea880c31d023c_t.jpeg"
-          alt="Recipe Image"
-          class="img-fluid rounded"
-        />
+        src="https://thumb.photo-ac.com/b9/b92f7ddb7e2f121217fea880c31d023c_t.jpeg"
+        alt="Recipe Image"
+        class="img-fluid rounded"
+      />
       <div class="mb-4">
         <h3 class="mb-4">{{ title2 }}</h3>
       </div>
+      <!-- CustomComponent を追加 -->
+      <CustomComponent />
       <div class="mb-4">
         <nuxt-link to="/" class="btn btn-primary">戻る</nuxt-link>
       </div>
     </div>
   </div>
 </template>
-
 <style scoped>
 
 </style>
